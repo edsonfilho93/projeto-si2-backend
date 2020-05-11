01753ce7573304af17ecf6c50ead95d91bd59b1d
Edson <edsonfilho734@gmail.com>
2020-05-10 22:21:09 -0300

    Criação do Projeto

---
+++ src/main/java/com/projetoSI/ProjetoSiApplication.java
@@ -0,0 +1,13 @@
+package com.projetoSI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoSI/ProjetoSiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoSI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
